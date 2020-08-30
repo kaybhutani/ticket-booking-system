262f1e502daa57dcc2f230632b196be3d5135612
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-08-30 17:02:20 +0530

    update iso format in model

--- utils/models.py
+++ utils/models.py
@@ -10,12 +10,12 @@ class RequestModels:
       'phoneNumber': fields.Integer('Phone number.'),
       'userName': fields.String('User Name'),
       'ticketCount': fields.Integer('Count of movie tickets to book.'),
-      'showTime': fields.String('Timing of show as in ISO format (YYYY:MM:DD HH:MM:SS)')
+      'showTime': fields.String('Timing of show as in ISO format (YYYY-MM-DD HH:MM:SS)')
     })
 
   def UpdateTimingModel(self):
     return self.api.model('UpdateTiming', {
-      'newShowTime': fields.String("New timing of show as in ISO format(YYYY:MM:DD HH:MM:SS)"),
+      'newShowTime': fields.String("New timing of show as in ISO format(YYYY-MM-DD HH:MM:SS)"),
       'ticketId': fields.String("Unique Ticket ID")
       })
 
