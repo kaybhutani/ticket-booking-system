d5bbded9988d06087b46df8662d9701a86135224
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-08-30 20:19:29 +0530

    add swagger content

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from utils.api import api
 
 app = Flask(__name__)
 
-flaskApp = Api(title='Zomentum Assessment - Ticket booking system', description='Made by Kartikay Bhutani\nRoll Number: 9917102214\nUse the following swagger to test the API\'s.' , version='1.0')
+flaskApp = Api(title='Zomentum Assessment - Ticket booking system', description='Made by Kartikay Bhutani\nRoll Number: 9917102214\n\n ### Use the following swagger to test the **API\'s**.\n- Click on the namespace (api).\n- Choose any endpoint you like and click on **Try it out**.\n- You can also see the serializers and Request models in **models**' , version='1.0')
 flaskApp.add_namespace(api)
 flaskApp.init_app(app)
 
